74e1af9605a40daa20f479ae8a4de94495f9e7b1
DivyanshiDetroja <divyanshi.m.detroja@gmail.com>
2024-02-29 00:12:21 +0530

    basic navbar added

--- snapgredient-client/src/App.jsx
+++ snapgredient-client/src/App.jsx
@@ -1,53 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [image, setImage] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  const handleImageChange = (event) => {
-    const file = event.target.files[0];
-    setImage(file);
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setLoading(true);
-
-    const formData = new FormData();
-    formData.append('image', image);
-
-    try {
-      const response = await fetch('http://localhost:8000/upload', {
-        method: 'POST',
-        body: formData
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        console.log('Image uploaded successfully:', data);
-      } else {
-        console.error('Failed to upload image:', response.statusText);
-      }
-    } catch (error) {
-      console.error('Error uploading image:', error);
-    }
-
-    setLoading(false);
-  };
-
   return (
-    <div>
-      <h2>Upload Image</h2>
-      <form onSubmit={handleSubmit}>
-        <input type="file" accept="image/*" onChange={handleImageChange} />
-        <button type="submit" disabled={!image || loading}>
-          {loading ? 'Uploading...' : 'Upload'}
-        </button>
-      </form>
+    <div style={{ width: "100vw" }}>
+      <Navbar />
+      Test App ok
     </div>
   );
 }
 
-
-export default App
+export default App;
---
+++ snapgredient-client/src/components/Navbar.jsx
@@ -0,0 +1,66 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import AdbIcon from "@mui/icons-material/Adb";
+
+function Navbar() {
+  return (
+    <AppBar
+      position="static"
+      sx={{ boxShadow: 0, backgroundColor: "transparent" }}
+    >
+      <Container
+        maxWidth="xl"
+        sx={{ display: "flex", justifyContent: "center" }}
+      >
+        <Toolbar disableGutters>
+          <AdbIcon
+            sx={{ display: { xs: "none", md: "flex" }, mr: 1, color: "black" }}
+          />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "black",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+
+          <AdbIcon
+            sx={{ display: { xs: "flex", md: "none" }, mr: 1, color: "black" }}
+          />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "black",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default Navbar;
